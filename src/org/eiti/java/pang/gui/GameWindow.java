beaa534144fce15c2ad0d83be1a70d7f661247be
Stefan Hennel <shennel@stud.elka.pw.edu.pl>
2015-11-17 15:32:12 +0100

    Pasek Menu

--- src/org/eiti/java/pang/gui/GameWindow.java
+++ src/org/eiti/java/pang/gui/GameWindow.java
@@ -2,7 +2,7 @@ package org.eiti.java.pang.gui;
 
 import java.awt.BorderLayout;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class GameWindow extends JFrame {
 
@@ -15,6 +15,46 @@ public class GameWindow extends JFrame {
 		
 		setLayout(new BorderLayout());
 		getContentPane().add(new GamePanel(), BorderLayout.CENTER);
+
+		JMenuBar menuBar = new JMenuBar();
+		JMenu gameMenu = new JMenu("Game");
+		JMenu settMenu = new JMenu("Settings");
+		JMenu helpMenu = new JMenu("Help");
+
+		setJMenuBar(menuBar);
+		menuBar.add(gameMenu);
+		menuBar.add(settMenu);
+		menuBar.add(helpMenu);
+
+		JMenuItem newGame = new JMenuItem("New Game CTRL+N");
+		gameMenu.add(newGame);
+		JMenuItem pause = new JMenuItem("Pause CTRL+P");
+		gameMenu.add(pause);
+		gameMenu.addSeparator();
+		//JMenuItem nickname = new JMenuItem("Nickname");
+		//gameMenu.add(nickname);
+		JMenuItem bestScores = new JMenuItem("Best Scores");
+		gameMenu.add(bestScores);
+		gameMenu.addSeparator();
+		JMenuItem quitGame = new JMenuItem("Quit CTRL+Q");
+		gameMenu.add(quitGame);
+
+		JMenuItem sound = new JMenuItem("Sound");
+		settMenu.add(sound);
+		JMenuItem mute = new JMenuItem("Mute");
+		settMenu.add(mute);
+		settMenu.addSeparator();
+		JMenuItem theme1 = new JMenuItem("Theme 1");
+		settMenu.add(theme1);
+		JMenuItem theme2 = new JMenuItem("Theme 2");
+		settMenu.add(theme2);
+		settMenu.addSeparator();
+		JMenuItem standardMode = new JMenuItem("Standard Mode");
+		settMenu.add(standardMode);
+		JMenuItem viMode = new JMenuItem("VI Mode");
+		settMenu.add(viMode);
+
+
 	}
 	
 }
